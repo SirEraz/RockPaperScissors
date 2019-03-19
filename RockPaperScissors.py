57721d9fcbe6ee6f87f2568f21f2fc43258b2871
Eraz <48658740+SirEraz@users.noreply.github.com>
2019-03-19 07:10:04 -0500

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -8,6 +8,7 @@ spacer = str("\n\n\n\n\n\n\n\n\n\n\n")
 
 print("Rock… Paper… Scissors…")
 print("1 - Rock \n2 - Paper\n3 - Scissors")
+print("Enter 1-3 to decide, or anything else to exit the game.")
 
 menu = int(input("DECIDE: "))
 
@@ -18,4 +19,4 @@ elif menu == 2:
 elif menu == 3:
 ##
 else:
-## 
+## break 
