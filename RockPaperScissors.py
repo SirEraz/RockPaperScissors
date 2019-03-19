c5e56e988396152d5f4b548c55abb2144f9d773d
Eraz <48658740+SirEraz@users.noreply.github.com>
2019-03-19 07:08:41 -0500

    Create RockPaperScissors.py

---
+++ RockPaperScissors.py
@@ -0,0 +1,21 @@
+import math
+import random
+
+playerHand = 0
+cpuHand = 0
+
+spacer = str("\n\n\n\n\n\n\n\n\n\n\n")
+
+print("Rock… Paper… Scissors…")
+print("1 - Rock \n2 - Paper\n3 - Scissors")
+
+menu = int(input("DECIDE: "))
+
+if menu == 1:
+##
+elif menu == 2:
+##
+elif menu == 3:
+##
+else:
+## 
