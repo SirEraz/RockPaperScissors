f1f0594c7710d569365eb3a36fc9b5063eb6995d
Eraz <48658740+SirEraz@users.noreply.github.com>
2019-03-19 08:06:15 -0500

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -10,12 +10,21 @@ print("Rock… Paper… Scissors…")
 print("1 - Rock \n2 - Paper\n3 - Scissors")
 print("Enter 1-3 to decide, or anything else to exit the game.")
 
-menu = int(input("DECIDE: "))
+menu = int(input(" DECIDE: "))
 
 if menu == 1:
-##
+## playerHand = 1
+## cpuHand = random.randint(1,3)
+## if cpuHand == playerHand:
+## ## ---
+## if cpuHand = 2:
+## ## ---
+## if cpuHand = 3:
+## ## ---
 elif menu == 2:
-##
+## playerHand = 2:
+## cpuHand = random.randint(1,3)
+## if cpuHand == playerHand
 elif menu == 3:
 ##
 else:
